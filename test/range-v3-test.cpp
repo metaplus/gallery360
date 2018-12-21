79264be01cb26706fab7b53560934d3190488c66
Jingxing Xu <jx.xu@outlook.com>
2018-12-21 14:05:26 +0800

    command - qp-based rate control

--- test/range-v3-test.cpp
+++ test/range-v3-test.cpp
@@ -5,5 +5,7 @@ namespace range_test
     TEST(Ints, Base) {
         auto i = ranges::view::ints(1, 5);
         EXPECT_EQ(ranges::size(i), 4);
+        EXPECT_EQ(ranges::min(i), 1);
+        EXPECT_EQ(ranges::max(i), 4);
     }
 }
